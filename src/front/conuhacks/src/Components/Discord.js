6f811d16ebbfd901ecf58ddd55d2d7a947bb7407
Whitewolf04 <tominhtuan18@gmail.com>
2023-01-21 15:53:29 -0500

    Creating frames for the frontend

---
+++ src/front/conuhacks/src/Components/Discord.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Discord extends React.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/front/conuhacks/src/Components/MSTeams.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class MSTeams extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="title">
+
+                </div>
+                <div className="content">
+                    
+                </div>
+            </div>
+        )
+    }
+}
