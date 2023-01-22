5e37291b5c9f9f4f856f575bd60199ee0b04106b
minhtrannhat <minh@minhtrannhat.com>
2023-01-22 02:41:30 -0500

    Fix(Frontend/Discord.js): Discord content text color and padding

--- src/front/conuhacks/src/Components/Discord.js
+++ src/front/conuhacks/src/Components/Discord.js
@@ -1,39 +1,45 @@
 import React from "react";
-import Box from '@mui/material/Box';
+import Box from "@mui/material/Box";
 
-
-class ChatFeed extends React.Component{
-    render() {
-        return (
-            <Box sx={{bgcolor: '#2C2F33', borderRadius: 0.9}}>
-                Content
-            </Box>
-        )
-    }
+class ChatFeed extends React.Component {
+  render() {
+    return (
+      <Box sx={{ p: 1, color: "white", bgcolor: "#2C2F33", borderRadius: 0.9 }}>
+        Content
+      </Box>
+    );
+  }
 }
 
 class Discord extends React.Component {
-    render() {
-        return (
-            <Box sx={{
-                borderRadius: 3,
-                width: '100 %',
-                height: '100 %',
-                bgcolor: 'transparent',
-                minHeight: {lg: 225}}}
-                style={{backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
-                    <Box sx={{
-                        textAlign: "center",
-                        fontWeight: 'bold',
-                        color: 'white',
-                        pt: 3,
-                    }}>Discord</Box>
-                    <Box sx={{pl: 2, pr: 2, pt: 2, pb: 1}}>
-                        <ChatFeed/>
-                    </Box>
-            </Box>
-        )
-    }
+  render() {
+    return (
+      <Box
+        sx={{
+          borderRadius: 3,
+          width: "100 %",
+          height: "100 %",
+          bgcolor: "transparent",
+          minHeight: { lg: 225 },
+        }}
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.3)" }}
+      >
+        <Box
+          sx={{
+            textAlign: "center",
+            fontWeight: "bold",
+            color: "white",
+            pt: 3,
+          }}
+        >
+          Discord
+        </Box>
+        <Box sx={{ pl: 2, pr: 2, pt: 2, pb: 1 }}>
+          <ChatFeed />
+        </Box>
+      </Box>
+    );
+  }
 }
 
 export default Discord;
