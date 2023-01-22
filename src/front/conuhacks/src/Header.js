fdc0e1874c2f58f250f716d7ff19a388a47c711b
Thanh Ta <59896258+mth-1012@users.noreply.github.com>
2023-01-22 03:23:19 -0500

    Update header color

--- src/front/conuhacks/src/Header.js
+++ src/front/conuhacks/src/Header.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 class Header extends React.Component{
     render() {
         return(
             <Box sx={{
-                bgcolor: "#5ABEF9",
+                // bgcolor: "#5ABEF9",
                 borderRadius: 1,
                 textAlign: 'center',
                 p: 3,
                 }}>
-                <Typography variant='h4' sx={{fontWeight: 'bold'}}>Mirego</Typography>
+                    <Typography variant='body1'
+                        sx={{fontWeight: 'bold'}} 
+                        color={'white'} 
+                        align='left'
+                    >
+                        MIREGO BROADCAST</Typography>
             </Box>
         )
     }
