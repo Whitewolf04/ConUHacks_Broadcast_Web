affead296e430c3f24b61bda84627f09abfb94db
Thanh Ta <59896258+mth-1012@users.noreply.github.com>
2023-01-22 01:19:32 -0500

    Changed response from twitter links to twitter embeds

--- src/back/twitter-impl.js
+++ src/back/twitter-impl.js
@@ -1,4 +1,5 @@
 import twitter from 'twitter-api-sdk';
+import axios from 'axios';
 import config from './config.js';
 
 const twitterClient = new twitter.Client(config.BEARER_TOKEN);
@@ -8,7 +9,13 @@ export const fetchLatestTweet = async () => {
       const response = await twitterClient.tweets.usersIdTweets(config.CLIENT_ID, {
           max_results: 5,
       });
-      return response.data.map((tweet) => `https://twitter.com/${config.CLIENT}/status/${tweet.id}`);
+      const urls = response.data.map((tweet) => `https://twitter.com/${config.CLIENT}/status/${tweet.id}`);
+      let embeds = [];
+      for await (const url of urls) {
+        const result = await axios.get(`https://publish.twitter.com/oembed?url=${url}`);
+        embeds.push(result.data.html);
+      }
+      return embeds;
   } catch (e) {
       console.log(e);
       return null;
